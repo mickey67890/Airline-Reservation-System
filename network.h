0c0d9b69821943fed5c3de54705b7d3d73014b71
DesclipsE <82636121+DesclipsE@users.noreply.github.com>
2021-04-27 12:57:26 +0700

    Update network.h

--- network.h
+++ network.h
@@ -1,8 +1,8 @@
 /********************************************************/
-/* Network header								  		*/
+/* Network header					*/
 /* Define the function to create and traverse a network	*/
-/* Apiravit Intharakanchit (Cheetah) 63070503457		*/
-/* PrincessConnectSummerKyaru							*/
+/* Apiravit Intharakanchit (Cheetah) 63070503457	*/
+/* PrincessConnectSummerKyaru				*/
 /********************************************************/
 #ifndef NETWORK_H
 #define NETWORK_H
@@ -12,7 +12,7 @@
 typedef struct _node
 	{
 	struct _node* left;				/* Left child of the node */
-	struct _node* right;			/* right child of thee node */
+	struct _node* right;				/* right child of thee node */
 	char city[32];					/* The city of the location, used as key */
 	char country[32];				/* The country of the location, used as secondary key */
 	} NODE_T;
@@ -28,19 +28,19 @@ typedef struct _tree
 /******************/
 typedef struct _flight
 	{
-	char flightNumber[6];			/* Flight code */
+	char flightNumber[6];				/* Flight code */
 	char airline[32];				/* Name of airline */
 	int hourArrive;					/* The hour */
 	int minuteArrive;				/* The minute of the hour */
 	int hourDepart;					/* The hour */
 	int minuteDepart;				/* The minute of the hour */
 	NODE_T* origin;					/* The origin of the flight */
-	NODE_T* destination;			/* The destination of the flight */
+	NODE_T* destination;				/* The destination of the flight */
 	int seat[31];					/* Number of seat left */
 	int duration;					/* Duration of the flight in minutes */
-	int price;						/* Price of the flight in baht */
+	int price;					/* Price of the flight in baht */
 	int nextDay;					/* Check if the flight ends on the next day*/
-	struct _flight* next;			/* Next flight in the list */
+	struct _flight* next;				/* Next flight in the list */
 	} FLIGHT_T;
 
 /* For the weight of this edge, use either duration or price variable of flights */
@@ -55,13 +55,13 @@ typedef struct _edge
 typedef struct _vertex
 	{
 	NODE_T* location;				/* The location of this vertex */
-	char* key;	
-	int dValue;						/* Total weight so far, for Dijkstra's algo */ 
-	int color;						/* Used for coloring the vertices during traversal */
-	struct _vertex* parent;			/* Pointer to parent vertex */  
-	struct _vertex* next;			/* Next vertex in the list */
-    EDGE_T* edgeHead;				/* Pointer to the head of the edges list */
-    EDGE_T* edgeTail;				/* pointer to the tail of the edges list */
+	char* key;					/* Key of the vertex */
+	int dValue;					/* Total weight so far, for Dijkstra's algo */ 
+	int color;					/* Used for coloring the vertices during traversal */
+	struct _vertex* parent;				/* Pointer to parent vertex */  
+	struct _vertex* next;				/* Next vertex in the list */
+    EDGE_T* edgeHead;					/* Pointer to the head of the edges list */
+    EDGE_T* edgeTail;					/* pointer to the tail of the edges list */
 	} VERTEX_T;
 /******************/
 
